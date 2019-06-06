d1089597c4559ce97a08fcb31b8b5f6fc26b6ef0
Felix Rabe <felix@rabe.io>
2019-06-06 21:25:44 +0200

    Start using proc_macro2, syn, quote

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,11 @@ extern crate proc_macro;
 
 #[proc_macro]
 pub fn rcstruct(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    input
+    let rcstruct: proc_macro2::TokenStream = syn::parse_macro_input!(input);
+
+    let output = quote::quote! {
+        #rcstruct
+    };
+
+    output.into()
 }
